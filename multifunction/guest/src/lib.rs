baaa1607eccdfa0d9a57eacc1e0eb8a2b5a77862
Chengcheng-S <87012234+Chengcheng-S@users.noreply.github.com>
2024-04-18 14:53:36 +0800

    format code

--- multifunction/guest/src/lib.rs
+++ multifunction/guest/src/lib.rs
@@ -1,6 +1,11 @@
 #![cfg_attr(feature = "guest", no_std)]
 #![no_main]
 
+extern crate alloc;
+use core::panic;
+
+use alloc::vec::Vec;
+
 #[jolt::provable]
 fn div(n: u32, m: u32) -> u32 {
     match m {
@@ -12,4 +17,18 @@ fn div(n: u32, m: u32) -> u32 {
 #[jolt::provable]
 fn mul(x: u32, y: u32) -> u32 {
     x + y
+}
+
+#[jolt::provable]
+fn alloc(n: u32) -> u32 {
+    match n {
+        0 => panic!("n must gaterthan zero"),
+        _ => {
+            let mut v = Vec::<u32>::new();
+            for i in 0..=n {
+                v.push(i);
+            }
+            v[(n - 1) as usize]
+        }
+    }
 }
--- multifunction/src/main.rs
+++ multifunction/src/main.rs
@@ -1,16 +1,24 @@
 pub fn main() {
-    let (prove_div, verify_div) = guest::build_div();
+    // let (prove_div, verify_div) = guest::build_div();
 
-    let (output, proof) = prove_div(50, 25);
-    let is_valid = verify_div(proof);
+    // let (output, proof) = prove_div(50, 25);
+    // let is_valid = verify_div(proof);
 
-    println!("output: {}", output);
-    println!("valid: {}", is_valid);
+    // println!("output: {}", output);
+    // println!("valid: {}", is_valid);
 
-    let (prove_mul, verify_mul) = guest::build_mul();
+    // let (prove_mul, verify_mul) = guest::build_mul();
 
-    let (output, proof) = prove_mul(50, 10);
-    let is_valid = verify_mul(proof);
+    // let (output, proof) = prove_mul(50, 10);
+    // let is_valid = verify_mul(proof);
+
+    // println!("output: {}", output);
+    // println!("valid: {}", is_valid);
+
+    let (prove_alloc, verify_alloc) = guest::build_alloc();
+
+    let (output, proof) = prove_alloc(50);
+    let is_valid = verify_alloc(proof);
 
     println!("output: {}", output);
     println!("valid: {}", is_valid);
