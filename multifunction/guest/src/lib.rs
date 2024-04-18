a06c3840d3b4a6fe7ba74617365453dcf9bc10da
Chengcheng-S <87012234+Chengcheng-S@users.noreply.github.com>
2024-04-18 14:27:11 +0800

    add jolt example function

---
+++ multifunction/guest/src/lib.rs
@@ -0,0 +1,15 @@
+#![cfg_attr(feature = "guest", no_std)]
+#![no_main]
+
+#[jolt::provable]
+fn div(n: u32 , m:u32) -> u32 {
+    match m{
+     0 => panic!("division by zero"),
+     _ => n / m
+    }
+}
+
+#[jolt::provable]
+fn mul(x: u32,y:u32)->u32{
+    x + y
+}
---
+++ multifunction/src/main.rs
@@ -0,0 +1,18 @@
+pub fn main() {
+    let (prove_div, verify_div) = guest::build_div();
+
+    let (output, proof) = prove_div(50,25);
+    let is_valid = verify_div(proof);
+
+    println!("output: {}", output);
+    println!("valid: {}", is_valid);
+
+    let (prove_mul,verify_mul) = guest::build_mul();
+
+    let (output, proof) = prove_mul(50,10);
+    let is_valid = verify_mul(proof);
+
+    println!("output: {}", output);
+    println!("valid: {}", is_valid);
+
+}
