3c8612487474129dcac5ad9624002e7029780bba
Chengcheng-S <87012234+Chengcheng-S@users.noreply.github.com>
2024-08-26 11:13:27 +0800

    add jolt wasm code

--- json/src/main.rs
+++ json/src/main.rs
@@ -9,7 +9,8 @@ pub fn main() {
             }"#;
     
     let (output, proof) = prove_parse(data_str.to_string());
-    proof.save_to_file("./proof.json").expect("save filed");
+    proof
+    .save_to_file("./proof.json").expect("save filed");
     let is_valid = verify_parse(proof);
     println!("name: {}, balance {}", output.account_name, output.balance);
     println!("valid: {}", is_valid);
---
+++ wasmcode/readme.md
@@ -0,0 +1,6 @@
+## wasm for jolt 
+
+```shell
+ld: symbol(s) not found for architecture arm64
+clang: error: linker command failed with exit code 1 (use -v to see invocation)
+```
